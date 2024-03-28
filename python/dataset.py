d64ccabad4e7e098e819d2537e28cb04a0079cf5
suitupid <suitupid@gmail.com>
2024-03-28 23:06:22 +0900

    encoder 변경: OnehotEncoder -> LabelEncoder

--- python/dataset.py
+++ python/dataset.py
@@ -8,19 +8,18 @@ from torch.utils.data import Dataset
 
 class CustomDataset(Dataset):
 
-    def __init__(self, datasets, target_encoder=None):
+    def __init__(self, datasets):
         self.datasets = datasets
-        self.target_encoder = target_encoder
 
     def __getitem__(self, index):
-        path, label = self.datasets[index]
-        _x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-        _x = _x.reshape(1, 64, 64)
-        _x = torch.from_numpy(np.array(_x, dtype=np.float32))
-        _y = self.target_encoder.transform([[label]])
-        _y = _y.reshape(-1)
-        _y = torch.from_numpy(np.array(_y, dtype=np.float32))
-        return _x, _y
+        fpath, label = self.datasets[index]
+        x = cv2.imread(fpath, cv2.IMREAD_GRAYSCALE)
+        x = x / 255
+        x = np.float32(x)
+        x = x.reshape(1, 128, 128)
+        x = torch.tensor(x)
+        y = torch.tensor(label)
+        return x, y
 
     def __len__(self):
         return len(self.datasets)
--- python/train.py
+++ python/train.py
@@ -7,23 +7,87 @@ import joblib
 from sklearn.model_selection import train_test_split
 import torch
 from torch.utils.data import DataLoader
-
 from lightning import Trainer
 from lightning.pytorch.callbacks.early_stopping import EarlyStopping
 from lightning.pytorch.callbacks import RichProgressBar
 
 from dataset import CustomDataset
-from model import CnnModel
+# from model import CnnModel
 
 data = json.loads(open('data/dataInfo.json','r').read())
-onehot_encoder = joblib.load('data/onehotEncoder.bin')
 
-train, valid = train_test_split(data, test_size=0.05, shuffle=True)
+train, valid = train_test_split(
+    data,
+    test_size=0.05, shuffle=True,
+    stratify=[item[1] for item in data]
+)
 
-train_dataset = CustomDataset(train, onehot_encoder)
-valid_dataset = CustomDataset(valid, onehot_encoder)
-train_dataloader = DataLoader(train_dataset, batch_size=32, num_workers=16)
-valid_dataloader = DataLoader(valid_dataset, batch_size=32, num_workers=16)
+train_dataset = CustomDataset(train)
+valid_dataset = CustomDataset(valid)
+train_dataloader = DataLoader(train_dataset, batch_size=16, num_workers=8)
+valid_dataloader = DataLoader(valid_dataset, batch_size=16, num_workers=8)
+
+for x, y in train_dataloader:
+    break
+
+from torch import nn
+import lightning as L
+from torch.optim import Adam
+
+class CnnModel(L.LightningModule):
+
+    def __init__(self):
+        super().__init__()
+
+        self.model = nn.Sequential(
+            nn.Conv2d(1, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Conv2d(128, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.Conv2d(256, 256, kernel_size=3, padding=1),
+            nn.BatchNorm2d(256),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2),
+            nn.Dropout(0.5),
+            nn.Flatten(),
+            nn.Linear(256*32*32, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 512),
+            nn.BatchNorm1d(512),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(512, 2350)
+        )
+
+    def forward(self, x):
+        return self.model(x)
+
+    def training_step(self, batch):
+        x, y = batch
+        y_hat = self.forward(x)
+        y = y.reshape(-1)
+        train_loss = nn.CrossEntropyLoss()(y_hat, y)
+        self.log('train_loss', train_loss, prog_bar=True)
+        return train_loss
+
+    def validation_step(self, batch):
+        x, y = batch
+        y_hat = self.forward(x)
+        y = y.reshape(-1)
+        val_loss = nn.CrossEntropyLoss()(y_hat, y)
+        self.log('val_loss', val_loss, prog_bar=True)
+        return val_loss
+
+    def configure_optimizers(self):
+        return Adam(self.parameters(), lr=1e-3)
 
 model = CnnModel()
 trainer = Trainer(
