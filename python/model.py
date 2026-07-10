30d24f522621dfac211a2676a19435147a10b709
suitupid <suitupid@gmail.com>
2026-07-10 20:30:30 +0900

    모델 구조 수정 및 구현 방법 변경

--- python/model.py
+++ python/model.py
@@ -5,77 +5,72 @@ import joblib
 
 from torch import nn
 from torch.optim import Adam
+from torchvision.models.resnet import BasicBlock
 import lightning as L
+
+
+class ResNetBlock(nn.Module):
+
+    def __init__(self, inplanes, planes, stride=1):
+        super().__init__()
+        
+        downsample = None
+        if stride != 1 or inplanes != planes:
+            downsample = nn.Sequential(
+                nn.Conv2d(inplanes, planes, 1, stride, bias=False),
+                nn.BatchNorm2d(planes),
+            )
+        self.block = BasicBlock(inplanes, planes, stride, downsample)
+
+    def forward(self, x):
+        return self.block(x)
+
 
 class CustomResNet(L.LightningModule):
 
     def __init__(self):
         super().__init__()
+
         encoder = joblib.load('data/labelEncoder.bin')
         num_classes = len(encoder.classes_)
-        
-        self.init =  nn.Sequential(
+
+        self.stem = nn.Sequential(
             nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2, padding=1)
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
         )
-        self.layer1 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(64),
-            nn.ReLU(inplace=True)
+        
+        self.resnet_layer = nn.Sequential(
+            ResNetBlock(64, 128, stride=2),
+            ResNetBlock(128, 256, stride=2),
+            ResNetBlock(256, 512, stride=2)
         )
-        self.layer2 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(128),
-            nn.ReLU(inplace=True)
-        )
-        self.layer3 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(256, 256, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(256),
-            nn.ReLU(inplace=True)
-        )
-        self.layer4 = nn.Sequential(
-            nn.Conv2d(256, 512, kernel_size=3, stride=2, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(512, 512, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.BatchNorm2d(512),
-            nn.ReLU(inplace=True)
-        )
+
         self.flatten = nn.Flatten()
-        self.fc = nn.Sequential(
-            nn.Linear(512*5*5, 512),
+
+        self.head = nn.Sequential(
+            nn.Linear(512*4*4, 512),
             nn.BatchNorm1d(512),
-            nn.Dropout(0.5, inplace=True),
-            nn.ReLU(inplace=True),
+            nn.ReLU(),
+            nn.Dropout(0.5),
             nn.Linear(512, num_classes)
         )
 
+        self.loss_fn = nn.CrossEntropyLoss(label_smoothing=0.1)
+
     def forward(self, x):
-        out = self.init(x)
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
+        out = self.stem(x)
+        out = self.resnet_layer(out)
         out = self.flatten(out)
-        out = self.fc(out)
+        out = self.head(out)
         return out
 
     def training_step(self, batch):
         x, y = batch
         y_hat = self.forward(x)
         y = y.reshape(-1)
-        train_loss = nn.CrossEntropyLoss()(y_hat, y)
+        train_loss = self.loss_fn(y_hat, y)
         self.log('train_loss', train_loss, prog_bar=True)
         return train_loss
 
@@ -83,7 +78,7 @@ class CustomResNet(L.LightningModule):
         x, y = batch
         y_hat = self.forward(x)
         y = y.reshape(-1)
-        val_loss = nn.CrossEntropyLoss()(y_hat, y)
+        val_loss = self.loss_fn(y_hat, y)
         self.log('val_loss', val_loss, prog_bar=True)
         return val_loss
 
